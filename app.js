061579b8a7006c3768781bb6d1fa5dceb0668af1
Ryan Henness <ryanhenness@gmail.com>
2019-08-01 18:29:12 -0700

    Added index route, and procfile.

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var Song = require('./SongSchema');
+var Song = require('./schemas/SongSchema');
+if (process.env.DATABASE_CONNECTION_URL == undefined)
+  require('./env.js');
 
-var db ='';
+var db = process.env.DATABASE_CONNECTION_URL;
 
 mongoose.connect(db, {useNewUrlParser: true});
+
+app.get('/', function(req, res){
+  res.send("Welcome to GoodMusicApi!");
+});
 
 app.get('/songs', function(req, res){
   Song.find({})
@@ -20,7 +26,7 @@ app.get('/songs', function(req, res){
     });
 });
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function(){
   console.log("GoodMusicApi is listening on port " + port);
 });
