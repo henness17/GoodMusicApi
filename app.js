2b1d5d08ba83ca53c797e385fec04ae58d1e024b
Ryan Henness <ryanhenness@gmail.com>
2019-08-02 13:28:32 -0700

    Moved songs routes to external file, added POST for song.

--- app.js
+++ app.js
@@ -4,61 +4,28 @@ var app = express();
 app.use(cors());
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var Song = require('./schemas/SongSchema');
 if (process.env.DATABASE_CONNECTION_URL == undefined)
   require('./env.js');
+var Song = require('./schemas/SongSchema');
 
 var db = process.env.DATABASE_CONNECTION_URL;
-
 mongoose.connect(db, {useNewUrlParser: true});
 
 mongoose.connection.on('error', function(err){  
   console.log("There was an error connectiong to the database: " + err);
 }); 
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.get('/', function(req, res){
   res.send("Welcome to GoodMusicApi!");
 });
 
-app.get('/songs', function(req, res){
-  Song.find({})
-    .exec(function(err, songs){
-      if (err){
-        res.send('An error occured finding songs.');
-      }else{
-        console.log("Songs found.");
-        res.json(songs);
-      }
-    });
-});
-
-app.get('/songs/genre/:genre', function(req, res){
-  Song.find({
-    genre: req.params.genre
-  })
-    .exec(function(err, songs){
-      if (err){
-        res.send('An error occured finding songs.');
-      }else{
-        console.log("Songs found.");
-        res.json(songs);
-      }
-    });
-});
-
-app.get('/songs/artist/:artist', function(req, res){
-  Song.find({
-    artist: req.params.artist
-  })
-    .exec(function(err, songs){
-      if (err){
-        res.send('An error occured finding songs.');
-      }else{
-        console.log("Songs found.");
-        res.json(songs);
-      }
-    });
-});
+var songsRoutes = require('./routes/songsRoutes.js');
+app.use('/songs', songsRoutes);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function(){
---
+++ routes/songsRoutes.js
@@ -0,0 +1,65 @@
+var Song = require('../schemas/SongSchema');
+module.exports = (function() {
+    'use strict';
+    var songsRoutes = require('express').Router();
+
+    songsRoutes.get('/', function(req, res){
+      Song.find({})
+        .exec(function(err, songs){
+          if (err){
+            res.send('An error occured finding songs.');
+          }else{
+            console.log("Songs found.");
+            res.json(songs);
+          }
+        });
+    });
+
+    songsRoutes.get('/genre/:genre', function(req, res){
+      Song.find({
+        genre: req.params.genre
+      })
+        .exec(function(err, songs){
+          if (err){
+            res.send('An error occured finding songs.');
+          }else{
+            console.log("Songs found.");
+            res.json(songs);
+          }
+        });
+    });
+
+    songsRoutes.get('/artist/:artist', function(req, res){
+      Song.find({
+        artist: req.params.artist
+      })
+        .exec(function(err, songs){
+          if (err){
+            res.send('An error occured finding songs.');
+          }else{
+            console.log("Songs found.");
+            res.json(songs);
+          }
+        });
+    });
+
+    songsRoutes.post('/song', function(req, res){
+      var newSong = new Song();
+
+      newSong.title = req.body.title;
+      newSong.artist = req.body.artist;
+      newSong.genre = req.body.genre;
+      newSong.url = req.body.url;
+
+      newSong.save(function(err, song){
+        if (err){
+          res.send('Error saving song: ' + err);
+        } else {
+          console.log(song);
+          res.send(song);
+        }
+      });
+    });
+
+    return songsRoutes;
+})();
